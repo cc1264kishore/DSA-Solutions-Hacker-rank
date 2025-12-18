13308b74dcfa4223cfff806ff9f9ded1396baf26
cc1264kishore <cc1264@srmist.edu.in>
2025-12-18 17:06:31 +0530

    Update insert_tail.py

--- DSA-Solutions-structure/linked_lists/insert_tail.py
+++ DSA-Solutions-structure/linked_lists/insert_tail.py
@@ -1,11 +1,107 @@
-# insert_tail.py
-# Add your solution code here
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-def solution():
-    """Write your solution logic here and use input()/print() if required."""
-    pass
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+      static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+    //Create the node to be inserted in the Linked List
+    SinglyLinkedListNode temp = new SinglyLinkedListNode(data);
+
+    //If head is null simply point head to temp and return head
+    if (head == null) {
+        return temp;
+    }
+
+    //If head is not null iterate through the Linked List using a temp Node: curr
+    SinglyLinkedListNode curr = head;
+
+    //Exit the while loop when curr.next points to null
+    while (curr.next != null) {
+        curr = curr.next;
+    }
+
+    //Add the temp node at curr.next
+    curr.next = temp;
+
+    //Return the head
+    return head;
+}
 
 
-if __name__ == '__main__':
-    # For testing locally, call solution()
-    pass
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        	
+          SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+
+          llist.head = llist_head;
+        }
+
+
+
+        printSinglyLinkedList(llist.head, "\n", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
