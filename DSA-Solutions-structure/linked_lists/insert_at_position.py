d2a93092f3f63a4e5541dd68b692d6c9bbe26652
cc1264kishore <cc1264@srmist.edu.in>
2025-12-18 17:09:35 +0530

    Update insert_at_specific_position.py

--- DSA-Solutions-structure/linked_lists/insert_at_position.py
+++ DSA-Solutions-structure/linked_lists/insert_at_position.py
@@ -1,11 +1,102 @@
-# insert_at_position.py
-# Add your solution code here
+#!/bin/python3
 
-def solution():
-    """Write your solution logic here and use input()/print() if required."""
-    pass
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'insertNodeAtPosition' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER data
+#  3. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def insertNodeAtPosition(llist, data, position):
+    # Write your code here
+    new_node = SinglyLinkedListNode(data)
+
+    # Case 1: Insert at head
+    if position == 0:
+        new_node.next = llist
+        return new_node
+
+    # Traverse to (position - 1)th node
+    curr = llist
+    index = 0
+
+    while index < position - 1 and curr is not None:
+        curr = curr.next
+        index += 1
+
+    # Insert in-between
+    new_node.next = curr.next
+    curr.next = new_node
+
+    return llist
 
 
 if __name__ == '__main__':
-    # For testing locally, call solution()
-    pass
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
