6c22994b625835c812eb45f8bf27741b76034783
cc1264kishore <cc1264@srmist.edu.in>
2025-12-18 17:48:42 +0530

    Update is_bst.py

--- DSA-Solutions-structure/trees/is_bst.py
+++ DSA-Solutions-structure/trees/is_bst.py
@@ -1,11 +1,58 @@
-# is_bst.py
-# Add your solution code here
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-def solution():
-    """Write your solution logic here and use input()/print() if required."""
-    pass
+def newNode():
+    temp = node(-1)
+    temp.left = None
+    temp.right = None
+    return(temp);
 
+""" Node is defined as
+class node:
+  def __init__(self, data):
+      self.data = data
+      self.left = None
+      self.right = None
+"""
 
-if __name__ == '__main__':
-    # For testing locally, call solution()
-    pass
+def check_binary_search_tree_(root):
+
+    def isBST(node, min_val, max_val):
+        if node is None:
+            return True
+        
+        if not (min_val < node.data < max_val):
+            return False
+        
+        return (isBST(node.left, min_val, node.data) and
+                isBST(node.right, node.data, max_val))
+    
+    return isBST(root, float('-inf'), float('inf'))
+
+ht = int(input())
+cnt = 0
+values = map(int, input().split(' '))
+values = list(values)
+root  = newNode()
+def inorder(root, ht):
+    global cnt
+    global values
+    if cnt == len(values):
+        return
+    else:
+        if(ht>0):
+            root.left = newNode();
+            inorder(root.left, ht-1);
+        root.data = values[cnt];
+        cnt+=1
+        if(ht>0):
+            root.right = newNode();
+            inorder(root.right, ht-1);
+inorder(root, ht);
+if(check_binary_search_tree_(root)):
+    print("Yes")
+else:
+    print("No")
